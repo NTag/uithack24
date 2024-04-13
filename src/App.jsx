f98832c0e9c1b0bc0b33a91d6a7fb201e4cc7df0
Basile Bruneau <basile@bruneau.email>
2024-04-13 11:41:58 +0200

    feat: add questions

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
   return (
     <>
       <S.Title>Welcome to Challenger25!</S.Title>
+      <S.Subtitle>On what do you want to work on?</S.Subtitle>
+      <S.Categories>
+        <S.Category>Social anxiety, coming out…</S.Category>
+        <S.Category>Leaving home, adaptation…</S.Category>
+        <S.Category>Love & relationships</S.Category>
+      </S.Categories>
     </>
   );
 }
--- src/App.styles.js
+++ src/App.styles.js
@@ -5,3 +5,23 @@ export const Title = styled.h1`
   margin: 16px;
   font-weight: 800;
 `;
+
+export const Subtitle = styled.h3`
+  text-align: center;
+  margin: 16px;
+  opacity: 0.7;
+`;
+
+export const Categories = styled.div`
+  margin: 64px 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const Category = styled.button`
+  border: 2px solid black;
+  padding: 8px 16px;
+  text-align: center;
+  border-radius: 8px;
+`;
