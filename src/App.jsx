1b8ef45c82d2f58be0c388d2da79e7c9b60cd429
Basile Bruneau <basile@bruneau.email>
2024-10-11 17:18:00 +0200

    feat: add new desktop layout for the demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,104 @@
 import { Route, HashRouter as Router, Switch } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import { ChallengePage } from "./pages/Challenge";
 import { DashboardPage } from "./pages/Dashboard";
-import { OnboardingHomePage } from "./pages/Onboarding/Home";
 import { FeedbackPage } from "./pages/Feedback";
-
+import { OnboardingHomePage } from "./pages/Onboarding/Home";
 import { OnboardingQuestionsPage } from "./pages/Onboarding/Questions";
+import imgConnect from "./assets/connect.png";
+import imgTitle from "./assets/title.svg";
+import styled from "styled-components";
+
+const useIsMobile = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return width < 768;
+};
+
+const Nav = () => (
+  <Router>
+    <Switch>
+      <Route path="/onboarding/:category/:step">
+        <OnboardingQuestionsPage />
+      </Route>
+      <Route path="/challenges/:id/feedback">
+        <FeedbackPage />
+      </Route>
+      <Route path="/challenges/:id">
+        <ChallengePage />
+      </Route>
+      <Route path="/dashboard">
+        <DashboardPage />
+      </Route>
+      <Route path="/">
+        <OnboardingHomePage />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+const DesktopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  padding: 32px 64px;
+  gap: 64px;
+`;
+
+const PhoneContainer = styled.div`
+  border: 8px solid white;
+  border-radius: 48px;
+  width: 450px;
+`;
+
+const DemoContainer = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > h3 {
+    font-weight: bold;
+    font-size: 24px;
+    text-decoration: underline;
+  }
+`;
+
+const ImgTitle = styled.img`
+  height: 256px;
+`;
+
+const ImgQRCode = styled.img`
+  height: 256px;
+  margin-top: 64px;
+`;
 
 export default function App() {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return <Nav />;
+  }
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/onboarding/:category/:step">
-          <OnboardingQuestionsPage />
-        </Route>
-        <Route path="/challenges/:id/feedback">
-          <FeedbackPage />
-        </Route>
-        <Route path="/challenges/:id">
-          <ChallengePage />
-        </Route>
-        <Route path="/dashboard">
-          <DashboardPage />
-        </Route>
-        <Route path="/">
-          <OnboardingHomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <DesktopContainer>
+      <DemoContainer>
+        <ImgTitle src={imgTitle} alt="QueerQuest" />
+        <h3>www.queerquest.ovh</h3>
+        <ImgQRCode src={imgConnect} alt="Connect to QueerQuest" />
+        <h4>Let’s connect!</h4>
+      </DemoContainer>
+      <PhoneContainer>
+        <Nav />
+      </PhoneContainer>
+    </DesktopContainer>
   );
 }
